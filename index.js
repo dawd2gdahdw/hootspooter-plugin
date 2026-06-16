1ea749c236c40f0fa44920593e20094fb07d2c17
dawd2gdahdw <mirthbeck9137@outlook.com>
2026-06-16 13:43:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-(function(r,o,d){"use strict";let t=[];function i(e){return typeof e=="string"?e.replace(/hootspooter/gi,"okay"):e}var u={onLoad(){const e=o.findByProps("Text");e?.Text?.render&&t.push(d.after("render",e.Text,function(c,n){return n?.props?.children&&(n.props.children=i(n.props.children)),n}))},onUnload(){t.forEach(function(e){return e()}),t=[]}};return r.default=u,Object.defineProperty(r,"__esModule",{value:!0}),r})({},vendetta.metro,vendetta.patcher);
+import { findByProps } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+
+let unpatch: (() => void)[] = [];
+
+function replaceText(val: any): any {
+    if (typeof val === "string") return val.replace(/hootspotter/gi, "okay");
+    if (Array.isArray(val)) return val.map(replaceText);
+    return val;
+}
+
+export default {
+    onLoad() {
+        const React = findByProps("createElement");
+        unpatch.push(after("createElement", React, (args, res) => {
+            if (res?.props?.children) {
+                res.props.children = replaceText(res.props.children);
+            }
+            return res;
+        }));
+    },
+    onUnload() {
+        unpatch.forEach(u => u());
+        unpatch = [];
+    }
+};
