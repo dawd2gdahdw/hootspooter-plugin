1afc45866f6bb4229d8d40c49b7ceacc943d5b6c
dawd2gdahdw <mirthbeck9137@outlook.com>
2026-06-16 14:36:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-(function(t,u,o){"use strict";let n=[];var a={onLoad(){const e=u.findByProps("getCurrentUser");e.getCurrentUser.bind(e),n.push(o.after("getCurrentUser",e,function(d,r){return r&&r.username==="hootspotter"?{...r,username:"okay"}:r}))},onUnload(){n.forEach(function(e){return e()}),n=[]}};return t.default=a,Object.defineProperty(t,"__esModule",{value:!0}),t})({},vendetta.metro,vendetta.patcher);
+import { findByName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+
+let unpatch: (() => void)[] = [];
+
+export default {
+    onLoad() {
+        const ProfileSheet = findByName("UserProfileSheet", false);
+        if (!ProfileSheet) return;
+        unpatch.push(after("default", ProfileSheet, (_, res) => {
+            if (!res?.props) return res;
+            const walk = (obj: any) => {
+                if (!obj) return;
+                if (typeof obj === "string") return;
+                if (obj.props?.children) {
+                    if (typeof obj.props.children === "string") {
+                        obj.props.children = obj.props.children.replace(/hootspotter/gi, "okay");
+                    } else if (Array.isArray(obj.props.children)) {
+                        obj.props.children.forEach(walk);
+                    } else {
+                        walk(obj.props.children);
+                    }
+                }
+            };
+            walk(res);
+            return res;
+        }));
+    },
+    onUnload() {
+        unpatch.forEach(u => u());
+        unpatch = [];
+    }
+};
